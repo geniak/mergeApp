a7f1736dfce66c062f51f1cf93af95da5c8035aa
geniak <dreampassenger@hanmail.net>
2017-02-15 21:36:11 +0900

    comment SQL query

--- mergeApp/Program.cs
+++ mergeApp/Program.cs
@@ -8,7 +8,6 @@ using System.Data.OleDb;
 // Microsoft ADO Ext. 6.0 DLL and Security
 using ADOX;
 
-
 namespace mergeApp
 {
     class Program
@@ -25,7 +24,7 @@ namespace mergeApp
             string txtDB = Console.ReadLine();
             Catalog myCatalog = new Catalog();
             string strProvider = "Provider=Microsoft.ACE.OLEDB.12.0;" +
-                                 "Data Source=C:/Users/교/Desktop/csharp/school/mergeApp/" + txtDB.ToString() + ".accdb";
+                                 "Data Source=\\" + txtDB.ToString() + ".accdb";
 
             try
             {
@@ -39,7 +38,7 @@ namespace mergeApp
 
 
             string connStr = "Provider=Microsoft.ACE.OLEDB.12.0;" +
-                                 "Data Source=C:/Users/교/Desktop/csharp/school/mergeApp/" + txtDB.ToString() + ".accdb; Persist Security Info=False";
+                                 "Data Source=\\" + txtDB.ToString() + ".accdb; Persist Security Info=False";
 
             OleDbConnection conn = new OleDbConnection(connStr);
             conn.Open();
@@ -60,7 +59,6 @@ namespace mergeApp
             conn.Close();
 
             Console.Read();
-
         }
 
         
@@ -100,7 +98,58 @@ namespace mergeApp
 				conn.Close();
 			}
 
-			Console.ReadLine();
         }
+
+		static void checkFile()
+		{
+
+		}
+
+		static void checkData()
+		{
+
+		}
+
+		// sql 명령어
+		// Database create/show
+		// mysql> CREATE DATABASE dbname;
+		// mysql> SHOW DATABASE;
+		// mysql> USE dbname; // declare specific database
+		// DROP DATABASE [IF EXISTS] dbname; // delete database
+
+		// Create & Show Table
+		// mysql> CREATE TABLE tablename (
+		// column_name1 INT PRIMARY KEY AUTO_INCREMENT,
+		// column_name2 VARCHAR(15) NOT NULL,
+		// column_name3 INT
+		// ) ENGINE=INNODB;
+		// mysql> SHOW TABLES;
+		// mysql> EXPLAIN tablesname;
+		// mysql> DESCRIBE tablename;
+		// mysql> RENAME TABLE tablename1 TO tablename2[, tablename3 TO tablename4];
+		// mysql> DROP TABLE [IF EXISTS] tablename;
+
+		// Insert
+		// mysql> INSERT INTO tablename VALUES(value1, value2, ...);
+		// mysql> INSERT INTO tablename (col1, col2, ...) VALUES(value1, value2, ...);
+
+		// Select
+		// mysql> SELECT col1, col2, ... FROM tablename;
+		// you can use * instead of col, it means select all column.
+		// mysql> SELECT col1 AS 'Name', col2 AS 'Score' FROM grade;
+		// mysql> SELECT * FROM tablename ORDER BY col1 DESC;
+		// mysql> SELECT col1, korean + math english AS 'Total Score' FROM tablename ORDER BY 'Total Score' ASC;
+		// DESC : descending order, ASC : ascending order;
+		// mysql> SELECT * FROM grade WHERE korean < 90;
+		// mysql> SELECT * FROM grade LIMIT 10;
+		// take 10 result from first
+		// mysql> SELECT * FROM grade LIMIT 100, 10;
+		// take 10 result from 100th. ( records are start from 0 )
+
+		// Update
+		// mysql> UPDATE tablename SET col1=newvalue WHERE condition
+
+		// Delete
+		// mysql> DELETE FROM tablename WHERE condition
     }
 }
